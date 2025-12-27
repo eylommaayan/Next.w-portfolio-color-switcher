9dbc3900802fd4d3665702e295e91dc4b68ee361
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2025-12-27 17:17:38 +0200

    האתרמה

--- app/sections/navbar/data.js
+++ app/sections/navbar/data.js
@@ -1,8 +1,8 @@
 const data = [
-  { id: 1, link: "#", title: "Home" },
-  { id: 2, link: "#about", title: "About" },
-  { id: 3, link: "#services", title: "Services" },
-  { id: 5, link: "#contact", title: "Contact" },
+  { id: 1, link: "#", title: "בית" },
+  { id: 2, link: "#about", title: "אודות" },
+  { id: 3, link: "#services", title: "שירותים" },
+  { id: 4, link: "#contact", title: "יצירת קשר" },
 ];
 
 export default data;
--- app/theme/Theme.jsx
+++ app/theme/Theme.jsx
@@ -1,32 +1,39 @@
-import Modal from "../components/Modal"
-import { primaryColors, backgroundColors } from "./data"
-import PrimaryColor from "./PrimaryColor"
-import BackgroundColor from './BackgroundColor';
-import './theme.css'
+import Modal from "../components/Modal";
+import { primaryColors, backgroundColors } from "./data";
+import PrimaryColor from "./PrimaryColor";
+import BackgroundColor from "./BackgroundColor";
+import "./theme.css";
 
 const Theme = () => {
   return (
     <Modal className="theme__modal">
-        <h3>Customize Your Theme</h3>
-        <small>Change the primary and background color to your preference.</small>
-        <div className="theme__primary-wrapper">
-            <h5>Primary Color</h5>
-            <div className="theme__primary-colors">
-                {
-                    primaryColors.map(pColor => <PrimaryColor key={pColor.className} className={pColor.className}/>)
-                }
-            </div>
-        </div>
-        <div className="theme__background-wrapper">
-            <h5>Background Color</h5>
-            <div className="theme__background-colors">
-                {
-                    backgroundColors.map(bColor => <BackgroundColor key={bColor.className} className={bColor.className} />)
-                }
-            </div>
-        </div>
-    </Modal>
-  )
-}
+      <h2 className="roomTitle">הדמיית צבעי קיר</h2>
+      <p className="roomSubtitle">
+        בחרו צבע וראו איך הוא משתלב עם החדר והרהיטים
+      </p>
 
-export default Theme
+      <div className="theme__primary-wrapper">
+        <h5>צבע עיקרי</h5>
+        <div className="theme__primary-colors">
+          {primaryColors.map((pColor) => (
+            <PrimaryColor key={pColor.className} className={pColor.className} />
+          ))}
+        </div>
+      </div>
+
+      <div className="theme__background-wrapper">
+        <h5>צבע רקע</h5>
+        <div className="theme__background-colors">
+          {backgroundColors.map((bColor) => (
+            <BackgroundColor
+              key={bColor.className}
+              className={bColor.className}
+            />
+          ))}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default Theme;
